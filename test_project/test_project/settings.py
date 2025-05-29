d62598132ef8813f00d59ea5d49c5fd91102e5f4
balaji <balajik1924@gmail.com>
2025-05-29 12:50:17 +0530

    Update settings.py

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -138,5 +138,5 @@ EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_FROM='noreply.jobsearcher@gmail.com'
 EMAIL_HOST_USER='noreply.jobsearcher@gmail.com'
-EMAIL_HOST_PASSWORD='ulezgpuxveqxulgo'
+EMAIL_HOST_PASSWORD='Your Password'
 EMAIL_PORT=587           
